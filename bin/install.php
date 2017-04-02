7ef91c07ad49fa2922accdc44e0142811d25c88d
sabri <sabri.elgueder@satoripop.com>
2017-04-02 02:49:34 +0200

    fix bug templating

--- bin/install.php
+++ bin/install.php
@@ -264,17 +264,37 @@ class Installer
             } else {
                 if (file_exists($dst . '/' . $iterator->getSubPathName())) {
 
-                  $contents = file_get_contents($dst . '/' . $iterator->getSubPathName());
-                  $contents2 = file_get_contents($file);
 
-                  $contents .= '/n/n/n' . $contents2;
-                  file_put_contents($dst . '/' . $iterator->getSubPathName(), $contents);
+
+                  if($iterator->getSubPathName() == 'autoload.php') {
+
+                    // Enable libraries template
+                    $contents = file_get_contents($dst . '/' . $iterator->getSubPathName());
+                    $contents = str_replace(
+                        '$autoload[\'libraries\'] = array();',
+                        '$autoload[\'libraries\'] = array(\'template\');',
+                        $contents
+                    );
+                    file_put_contents($file, $contents);
+
+                  } else {
+
+                    $contents = file_get_contents($dst . '/' . $iterator->getSubPathName());
+                    $contents2 = file_get_contents($file);
+                    $contents .=  str_replace( '<?php', '', $contents2 );
+                    file_put_contents($dst . '/' . $iterator->getSubPathName(), $contents);
+
+                  }
+
+                  $success = false;
 
                 } else {
                   $success = copy($file, $dst . '/' . $iterator->getSubPathName());
                 }
                 if ($success) {
                     echo 'copied: ' . $dst . '/' . $iterator->getSubPathName() . PHP_EOL;
+                } else {
+                  echo 'updated: ' . $dst . '/' . $iterator->getSubPathName() . PHP_EOL;
                 }
             }
         }
